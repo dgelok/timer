ca5dbadbf37ab4b35cb69e545a0c7475d34cfa29
Dan Gelok <dgelok@gmail.com>
2020-05-28 16:18:55 -0500

    added time parser

--- timer.js
+++ timer.js
@@ -1,24 +1,42 @@
-var starttime = new Date();
+// get starting time
+var starttime = new Date("May 28, 2020 9:00:00");
 
 // get the project from a prompt
+var myProject = "Timer";
 
 // wait for the end time
 
-var endtime = new Date();
-var totalTime = endtime.getTime() - starttime.getTime();
+// get ending time
+var endtime = new Date("May 28, 2020 10:16:30");
+var totalTimeInMs = endtime.getTime() - starttime.getTime();
+var value = totalTimeInMs / 1000;
 
-// get days
-var myDays = totalTime / (1000 * 60 * 60 * 24);
-myDays = Math.floor(myDays);
-// get hours
-var myHours = totalTime - (myDays * 1000 * 60 * 60 * 24);
-myHours = Math.floor(myHours);
-// get minutes
-var myMinutes = totalTime - (myDays * 1000 * 60 * 60 * 24) - (myHours * 1000 * 60 * 60)
-myMinutes = Math.floor(myMinutes)
-// get seconds
+// parse in to hours, minutes, etc.
+var nDays = 0;
+var nHours = 0;
+var nMinutes = 0;
+var nSeconds = 0;
 
-// print out to user what their time today was
+if (value > 86400) {
+    nDays = Math.floor(value/86400);
+    value = value % 86400;
+}
+if (value > 3600) {
+    nHours = Math.floor(value/3600);
+    value = value % 3600;
+}
+if (totalTimeInMs > 60) {
+    nMinutes = Math.floor(value/60);
+    value = value % 60
+}
+nSeconds = Math.floor(value);
+
+
+// log the time to the console
+var s = `Today, you spent 
+\t${nDays} days, ${nHours} hours, ${nMinutes} minutes, and ${nSeconds} seconds... 
+\ton ${myProject}.`
+console.log(s);
 
 // open CSV
 // if project nonexistant, create it
